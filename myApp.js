a6d3d4d0e279b73e1e701e510551a1c013ec78c5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-05-19 17:57:21 +0100

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -134,7 +134,7 @@ const queryChain = async (done) => {
     const foodToSearch = "burrito";
 
     const people = await Person.find({ favoriteFoods: foodToSearch })
-      .sort({ name: -1 })
+      .sort({ name: 1 })
       .limit(2)
       .select("-age")
       .exec(done(err, people));
