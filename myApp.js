83482c049a3ed99e4596669c6eaa7ec0279deabd
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-05-19 13:32:02 +0100

    Connect database

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
+const mongoose = require("mongoose");
 require("dotenv").config({
-  path: "./sample.env",
+  path: "./env",
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 let Person;
