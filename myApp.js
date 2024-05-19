cb8323ef4e6d8507c416e43348f2db4a6caf209f
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-05-19 13:46:52 +0100

    Delete console.log()

--- myApp.js
+++ myApp.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-console.log(process.env.MONGO_URI);
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
